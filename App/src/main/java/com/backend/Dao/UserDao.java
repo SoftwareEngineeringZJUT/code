49e01faafa6a91f2288c6da06dae2e1e28b910c3
nuoya <1436458099@qq.com>
2022-12-01 16:31:38 +0800

    后端

--- App/src/main/java/com/backend/Dao/UserDao.java
+++ App/src/main/java/com/backend/Dao/UserDao.java
@@ -11,19 +11,17 @@ import java.util.List;
 @Mapper
 public interface UserDao {
     @Select("select * from user")
-    List<User> getAll();
-    //获取全部
+    List<User> getAll();//获取全部
     @Select("select * from user where account = #{account}")
-    User getUserByAccount(String account);
-    //根据账户account获取密码
+    User getUserByAccount(String account);//根据账户account获取用户
+    @Select("select select * from user where uid = #{uid}")
+    User getUserByUid(String uid);//根据账户account获取用户
     @Select("select password from user where account = #{account}")
-    User getPswdByAccout(String account);
-    //根据账户获取uid
+    User getPswdByAccout(String account);//根据账户获取密码
     @Select("select uid from user where account = #{account}")
-    User getUidByAccout(String account);
-    //插入新账户
-    @Insert("insert into user(uid,account,password,real_name,id_card,address,bank_card,phone,user_status,balance,label,gmt_create,gmt_update)\n" +
-            "values(NULL, #{account}, #{password}, #{real_name}, #{id_card}, #{address}, #{bank_card}, #{phone},#{user_status}, 0, #{label}, 'nFvZ', '', '')")
-    void insertUser();
+    User getUidByAccout(String account);//根据账户获取uid
+    @Insert("insert into user(uid,account,password,real_name,id_card,address,bank_card,phone,user_status,balance,label)\n" +
+            "values(NULL, #{account}, #{password}, #{real_name}, #{id_card}, #{address}, #{bank_card}, #{phone},#{user_status}, 0, #{label})")
+    void insertUser(User user);//插入新账户
 
 }
