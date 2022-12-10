47cdf38e8260315c151221af5234cac3b214b57a
nuoya <1436458099@qq.com>
2022-12-10 11:02:34 +0800

    订单信息管理

--- App/src/main/java/com/app/controller/OrderInfoController.java
+++ App/src/main/java/com/app/controller/OrderInfoController.java
@@ -1,8 +1,12 @@
 package com.app.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.app.dao.AdminDao;
 import com.app.dao.OrderDao;
+import com.app.dao.UserDao;
+import com.app.domain.Admin;
 import com.app.domain.Order;
+import com.app.domain.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +29,10 @@ public class OrderInfoController {
 
     @Resource
     private OrderDao orderDao;
-
+    @Resource
+    private AdminDao adminDao;
+    @Resource
+    private UserDao userDao;
     @PostMapping("/addOrder")
     public String addOrder(Order order){
         orderDao.InsertOrder(order);
@@ -52,17 +59,50 @@ public class OrderInfoController {
         return retJSON;
     }
 
-    @PostMapping("/queryOrder")
-    public String queryOrder(){
-        String retJSON;
+    /**
+     * 查询订单
+     *      管理员查询
+     * @param _admin
+     * @return retJSON
+     */
+    @PostMapping("/AdminQueryOrder")
+    public String AdminQueryOrder(Admin _admin){
+        String retJSON = "[]";
+        Admin admin = adminDao.getAdminByAccount(_admin.getAccount());
 
+        // 用户不存在
+        if(admin == null){
+            retJSON = addKeyValue(retJSON , "status" , "ADMIN_NOT_FOUND");
+            return retJSON;
+        }
         List<Order> orderList = orderDao.GetAllOrder();
-
         retJSON = JSON.toJSONString(orderList);
+        retJSON = addKeyValue(retJSON , "status" , "APPROVED");
 
         return retJSON;
     }
+    /**
+     * 查询订单
+     *      用户查询
+     * @param _user
+     * @return retJSON
+     */
+    @PostMapping("/UserQueryOrder")
+    public String UserQueryOrder(User _user){
+        String retJSON = "[]";
+        User user = userDao.getUserByAccount(_user.getAccount());
 
+        // 用户不存在
+        if(user == null){
+            retJSON = addKeyValue(retJSON , "status" , "USER_NOT_FOUND");
+            return retJSON;
+        }
+        List<Order> orderList = orderDao.GetByUserId(user.getUid());
+        retJSON = JSON.toJSONString(orderList);
+        retJSON = addKeyValue(retJSON , "status" , "APPROVED");
+
+        return retJSON;
+    }
     @PostMapping("/updateOrder")
     public String updateOrder(Order order){
         String retJSON = "{}";
